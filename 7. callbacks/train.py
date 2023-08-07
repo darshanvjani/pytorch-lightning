0e10e936e885f73ba7f77dca1249bc308b8b6042
Darshan Jani <35656144+darshanvjani@users.noreply.github.com>
2023-08-06 20:30:13 -0700

    custom callback implementation

---
+++ 7. callbacks/train.py
@@ -0,0 +1,34 @@
+!pip install pytorch_lightning
+
+import torch
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch import nn, optim
+from torch.utils.data import DataLoader,random_split
+from tqdm import tqdm
+
+import pytorch_lightning as pl
+import torchmetrics
+from torchmetrics import Metric
+
+import config
+from model import NN
+from dataset import MNISTDataModule
+import callbacks
+
+if __name__ == '__main__':
+    model = NN(input_size=config.INPUT_SIZE, learning_rate=config.LEARNING_RATE, num_classes=config.NUM_CLASSES)
+    
+    
+    dm = MNISTDataModule(
+      data_dir=config.DATA_DIR,
+      batch_size=config.BATCH_SIZE,
+      num_workers=config.NUM_WORKERS
+    )
+    
+    
+    trainer = pl.Trainer(accelerator=config.ACCELERATOR, devices=config.DEVICES, max_epochs=config.NUM_EPOCHS, precision=config.PRECISION, callbacks=[callbacks.MyPrintingCallback(), callbacks.EarlyStopping(monitor="val_loss")]) 
+    trainer.fit(model, dm)
+    trainer.validate(model, dm)
+    trainer.test(model, dm)
